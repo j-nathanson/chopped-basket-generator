1766d71c152e428702708f02893b692ff78cbd45
j-nathanson <jacobnath93@gmail.com>
2021-07-15 16:16:05 -0400

    created the basket ingredient objects and filled in key-value pairs

--- script.js
+++ script.js
@@ -3,4 +3,54 @@ const randomOutOfTen = () => {
   return Math.floor(Math.random() * 10);
 };
 
-console.log(randomOutOfTen());
+const basketIngredients = {
+  proteins: [
+    "a whole suckling pig",
+    "duck sausage",
+    "rotisserie chicken",
+    "ostrich tenderloin",
+    "grouper fish",
+    "guinea hen",
+    "blood clams",
+    "rack of vension",
+    "smoked salmon",
+    "goat chops",
+  ],
+  carbs: [
+    "corn tortillas",
+    "couscous",
+    "star pasta",
+    "anzac biscuits",
+    "broken rice",
+    "potato tots",
+    "fruit ring cereal",
+    "dumpling wrappers",
+    "pizza dough",
+    "ramen noodles",
+  ],
+  vegetables: [
+    "fiddlehead ferns",
+    "sauerkraut",
+    "cauliflower",
+    "frilly mustard greens",
+    "nopales",
+    "morning glory",
+    "frozen spinach",
+    "broccoli rabe",
+    "fingerling potatoes",
+    "romanesco",
+    "sunchoke",
+  ],
+  random: [
+    "red and white kabob sauces",
+    "carrot ketchup",
+    "kola nut syrup",
+    "pork rinds",
+    "head cheese",
+    "twinkees",
+    "quail eggs",
+    "curry powder",
+    "pickled green tomatoes",
+    "dried plums",
+  ],
+};
